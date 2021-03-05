ca103edde03997a64d23b78cc477d0443a87ab45
TangleSpace <lenardeuler@hotmail.com>
2021-03-05 12:09:42 +1100

    updates

--- hotstepper/samples/__init__.py
+++ hotstepper/samples/__init__.py
@@ -8,6 +8,22 @@ warnings.filterwarnings("ignore")
 
 DATA_DIR = os.path.join(os.path.dirname(__file__),'data')
 
+# def _download_datasets_if_missing():
+#     #Handy code from the Seaborn package to manage the datasets through git and a local cache.
+
+#     path = ("https://raw.githubusercontent.com/"
+#                 "mwaskom/seaborn-data/master/{}.csv")
+#         full_path = path.format(name)
+
+#         if cache:
+#             cache_path = os.path.join(get_data_home(data_home),
+#                                     os.path.basename(full_path))
+#             if not os.path.exists(cache_path):
+#                 if name not in get_dataset_names():
+#                     raise ValueError(f"'{name}' is not one of the example datasets.")
+#                 urlretrieve(full_path, cache_path)
+#             full_path = cache_path
+
 def page_view_sample():
     df = pd.read_csv(os.path.join(DATA_DIR,'page_views.csv'))
     return Steps.read_dataframe(df,'start','ends')
