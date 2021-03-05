42343255f09f6982cb9de3410e934401f1bd9989
TangleSpace <lenardeuler@hotmail.com>
2021-03-06 08:32:04 +1100

    cleanup

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name='hotstepper',
-    version='1.0.2',    
+    version='1.0.3',    
     description='Numpy based step function library.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/tanglespace/hotstepper',
     author='Jackson Storm',
     author_email='c6lculus8ntr0py@gmail.com',
